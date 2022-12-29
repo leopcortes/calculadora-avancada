627c9fe1a324883702ab0498412e87d3188fad61
Leonardo <81263762+leopcortes@users.noreply.github.com>
2022-12-28 21:06:28 -0300

    arrumando calc iphone

--- js/iphone.js
+++ js/iphone.js
@@ -1,117 +1,184 @@
-/* ============ Calculadora iPhone ============ */
+/* ========== Calculadora iPhone ========== */
 
-const numeros = document.querySelectorAll(".numeros");
-const resultado = document.querySelector(".resultado");
-const sinais = document.querySelectorAll(".sinal");
-const igual = document.querySelector(".igual");
-const limpar = document.querySelector(".limpar");
-const negativo = document.querySelector(".negativo");
-const porcentagem = document.querySelector(".porcentagem");
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+const valueEl = document.querySelector('.value');
 
-let primeiroValor = "";
-let ehPrimeiroValor = false;
-let segundoValor = "";
-let ehSegundoValor = false;
-let sinal = "";
-let valorResultado = 0;
+const acEl = document.querySelector('.ac');
+const pmEl = document.querySelector('.pm');
+const percentEl = document.querySelector('.percent');
 
-for(let i = 0; i < numeros.length; i++){
-    numeros[i].addEventListener('click', (e) => {
-        let atr = e.target.getAttribute('value');
-        if(ehPrimeiroValor === false){
-            pegarPrimeiroValor(atr);
-        }
-        if(ehSegundoValor === false){
-            pegarSegundoValor(atr);
-        }
-    })
+const additionEl = document.querySelector('.addition');
+const subtractionEl = document.querySelector('.subtraction');
+const multiplicationEl = document.querySelector('.multiplication');
+const divisionEl = document.querySelector('.division');
+const equalEl = document.querySelector('.equal');
+
+const decimalEl = document.querySelector('.decimal');
+const number0El = document.querySelector('.number-0');
+const number1El = document.querySelector('.number-1');
+const number2El = document.querySelector('.number-2');
+const number3El = document.querySelector('.number-3');
+const number4El = document.querySelector('.number-4');
+const number5El = document.querySelector('.number-5');
+const number6El = document.querySelector('.number-6');
+const number7El = document.querySelector('.number-7');
+const number8El = document.querySelector('.number-8');
+const number9El = document.querySelector('.number-9');
+const numberElArray = [
+  number0El, number1El, number2El, number3El, number4El,
+  number5El, number6El, number7El, number8El, number9El
+];
+
+
+// variables
+let valueStrInMemory = null;
+let operatorInMemory = null;
+
+
+// Functions
+const getValueAsStr = () => valueEl.textContent.split(',').join('');
+
+const getValueAsNum = () => {
+  return parseFloat(getValueAsStr());
+};
+
+const setStrAsValue = (valueStr) => {
+  if (valueStr[valueStr.length - 1] === '.') {
+    valueEl.textContent += '.';
+    return;
+  }
+
+  const [wholeNumStr, decimalStr] = valueStr.split('.');
+  if (decimalStr) {
+    valueEl.textContent =
+      parseFloat(wholeNumStr).toLocaleString() + '.' + decimalStr;
+  } else {
+    valueEl.textContent = parseFloat(wholeNumStr).toLocaleString();
+  }
+};
+
+const handleNumberClick = (numStr) => {
+  const currentValueStr = getValueAsStr();
+  if (currentValueStr === '0') {
+    setStrAsValue(numStr);
+  } else {
+    setStrAsValue(currentValueStr + numStr);
+  }
+};
+
+const getResultOfOperationAsStr = () => {
+  const currentValueNum = getValueAsNum();
+  const valueNumInMemory = parseFloat(valueStrInMemory);
+  let newValueNum;
+  if (operatorInMemory === 'addition') {
+    newValueNum = valueNumInMemory + currentValueNum;
+  } else if (operatorInMemory === 'subtraction') {
+    newValueNum = valueNumInMemory - currentValueNum;
+  } else if (operatorInMemory === 'multiplication') {
+    newValueNum = valueNumInMemory * currentValueNum;
+  } else if (operatorInMemory === 'division') {
+    newValueNum = valueNumInMemory / currentValueNum;
+  }
+
+  return newValueNum.toString();
+};
+
+const handleOperatorClick = (operation) => {
+  const currentValueStr = getValueAsStr();
+
+  if (!valueStrInMemory) {
+    valueStrInMemory = currentValueStr;
+    operatorInMemory = operation;
+    setStrAsValue('0');
+    return;
+  }
+  valueStrInMemory = getResultOfOperationAsStr();
+  operatorInMemory = operation;
+  setStrAsValue('0');
+};
+
+
+
+
+// Add Event Listeners to functions
+acEl.addEventListener('click', () => {
+  setStrAsValue('0');
+  valueStrInMemory = null;
+  operatorInMemory = null;
+});
+pmEl.addEventListener('click', () => {
+  const currentValueNum = getValueAsNum();
+  const currentValueStr = getValueAsStr();
+
+  if (currentValueStr === '-0') {
+    setStrAsValue('0');
+    return;
+  }
+  if (currentValueNum >= 0) {
+    setStrAsValue('-' + currentValueStr);
+  } else {
+    setStrAsValue(currentValueStr.substring(1));
+  }
+});
+percentEl.addEventListener('click', () => {
+  const currentValueNum = getValueAsNum();
+  const newValueNum = currentValueNum / 100;
+  setStrAsValue(newValueNum.toString());
+  valueStrInMemory = null;
+  operatorInMemory = null;
+});
+
+
+// add event listeners to operators
+additionEl.addEventListener('click', () => {
+  handleOperatorClick('addition');
+});
+subtractionEl.addEventListener('click', () => {
+  handleOperatorClick('subtraction');
+});
+multiplicationEl.addEventListener('click', () => {
+  handleOperatorClick('multiplication');
+});
+divisionEl.addEventListener('click', () => {
+  handleOperatorClick('division');
+});
+equalEl.addEventListener('click', () => {
+  if (valueStrInMemory) {
+    setStrAsValue(getResultOfOperationAsStr());
+    valueStrInMemory = null;
+    operatorInMemory = null;
+  }
+});
+
+
+// Add Event Listeners to numbers and decimal
+for (let i=0; i < numberElArray.length; i++) {
+  const numberEl = numberElArray[i];
+  numberEl.addEventListener('click', () => {
+    handleNumberClick(i.toString());
+  });
 }
+decimalEl.addEventListener('click', () => {
+  const currentValueStr = getValueAsStr();
+  if (!currentValueStr.includes('.')) {
+    setStrAsValue(currentValueStr + '.');
+  }
+});
 
-function pegarPrimeiroValor(el){
-    resultado.innerHTML = "";
-    primeiroValor += el;
-    resultado.innerHTML = primeiroValor;
-    primeiroValor = +primeiroValor;
+
+// Set up the time
+const updateTime = () => {
+  const currentTime = new Date();
+
+  let currentHour = currentTime.getHours();
+  const currentMinute = currentTime.getMinutes();
+
+  if (currentHour > 12) {
+    currentHour -= 12;
+  }
+  hourEl.textContent = currentHour.toString();
+  minuteEl.textContent = currentMinute.toString().padStart(2, '0');
 }
-
-function pegarSegundoValor(el){
-    if(primeiroValor != "" && sinal != ""){
-        segundoValor += el;
-        resultado.innerHTML = segundoValor;
-        segundoValor = +segundoValor;
-    }
-}
-
-function pegarSinal(){
-    for(let i = 0; i < sinais.length; i++){
-        sinais[i].addEventListener('click', (e) => {
-            sinal = e.target.getAttribute('value');
-            ehPrimeiroValor = true;
-        })
-    }
-}
-pegarSinal();
-
-igual.addEventListener('click', () => {
-    resultado.innerHTML = "";
-    if(sinal === "+"){
-        valorResultado = primeiroValor + segundoValor;
-    } else if(sinal === "-"){
-        valorResultado = primeiroValor - segundoValor;
-    } else if(sinal === "x"){
-        valorResultado = primeiroValor * segundoValor;
-    } else if(sinal === "/"){
-        valorResultado = primeiroValor / segundoValor;
-    } 
-    resultado.innerHTML = valorResultado;
-    primeiroValor = valorResultado;
-    segundoValor = "";
-
-    checarComprimentoResultado();
-})
-
-function checarComprimentoResultado(){
-    valorResultado = JSON.stringify(valorResultado);
-
-    if(valorResultado.length >= 0) {
-        valorResultado = JSON.parse(valorResultado);
-        resultado.innerHTML = valorResultado.toFixed(5);
-    }
-}
-
-negativo.addEventListener('click', () => {
-    resultado.innerHTML = "";
-    if(primeiroValor != ""){
-        valorResultado = -primeiroValor;
-        primeiroValor = valorResultado;
-    }
-    if(primeiroValor != "" && segundoValor != "" && sinal != ""){
-        valorResultado = -valorResultado;
-    }
-
-    resultado.innerHTML = valorResultado;
-})
-
-porcentagem.addEventListener('click', () => {
-    resultado.innerHTML = "";
-    if(primeiroValor != ""){
-        valorResultado = primeiroValor / 100;
-        primeiroValor = valorResultado;
-    }
-    if(primeiroValor != "" && segundoValor != "" && sinal != ""){
-        valorResultado = valorResultado / 100;
-    }
-
-    resultado.innerHTML = valorResultado;
-})
-
-limpar.addEventListener('click', () => {
-    resultado.innerHTML = 0;
-    
-    primeiroValor = "";
-    ehPrimeiroValor = false;
-    segundoValor = "";
-    ehSegundoValor = false;
-    sinal = "";
-    valorResultado = 0;
-})
+setInterval(updateTime, 1000);
+updateTime();
